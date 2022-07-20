a18f44d249b8d323ccd928d580adc9c239d06813
zain <zan@deltalabs.io>
2022-07-20 22:20:27 +0500

    till now

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def show 
         
     end
+
     def index 
-        @articles = Article.all
-    end
+        @articles = Article.paginate(page: params[:page], per_page: 5)
+        end
 
     def new
         @article = Article.new
@@ -13,41 +17,46 @@ class ArticlesController < ApplicationController
     end
     
     def edit 
-      
+
     end
 
     def create
-    @article = Article.new(article_params)
-    @article.user = User.first
-    if @article.save
-        flash[:notice] = "Article was created successfully"
-        redirect_to @article
-    else
-           render 'new'
+        @article = Article.new(article_params)
+        @article.user = current_user
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+            redirect_to @article
+            else
+            render 'new'
+        end
     end
- end
- 
- def update
-    if @article.update(article_params)
- flash[:notice] = "Article was updated successfully"
- redirect_to @article
-    else
-        render "edit"
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+            else
+            render "edit"
+        end
     end
-   end
-def destroy
+    def destroy
     @article.destroy
     redirect_to articles_path
-end
+    end
 
-private 
+    private 
 
-def set_article
-@article = Article.find(params[:id])
-end
+    def set_article
+    @article = Article.find(params[:id])
+    end
 
-def article_params
-    params.require(:article).permit(:title,:description)
-end
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
 
+    def require_same_user
+        if current_user != @article.user && !current_user.admin?
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
+    end
 end
